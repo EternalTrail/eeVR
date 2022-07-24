f4d3ebd2ba36154c16b361d8515fa7650aa6cd20
SAM-tak <osamu.takasugi@gmail.com>
2022-07-24 18:15:04 +0900

    improve

--- package.sh
+++ package.sh
@@ -1,2 +1,8 @@
 rm -f eeVR.zip
-git archive HEAD  --output eeVR.zip
+git archive HEAD --output eeVR.tar
+mkdir eeVR
+tar -xvf eeVR.tar -C eeVR
+zip -FSr eeVR.zip eeVR
+rm -vr eeVR
+rm -f eeVR.tar
+mkdir eeVR
